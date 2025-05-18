c64e2ead4e5ecba1ab956bd33d8837dd8274b028
Priti Dwivedi <pritidwivedi@Pritis-MacBook-Air.local>
2025-05-18 23:55:50 +0530

    git ignore commit

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup , find_packages
+from typing import List
 
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+       
+    return requirements
 setup(
     name = 'CryptocurrencyLiquidityPrediction',
     version = '0.0.1',
     author = 'Priti',
     author_email = 'rishu07dwivedi@gmail.com',
-    packages = find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires = []
+    install_requires = get_requirements('requirements.txt'),
+    packages = find_packages()
+   
 )
